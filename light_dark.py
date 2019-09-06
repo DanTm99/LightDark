71cb41ba9228298f4bca04d6d8f12e4ce04c38c9
DanTm99 <danyaal99@hotmail.co.uk>
2019-09-06 21:20:49 +0100

    Initial commit

---
+++ light_dark.py
@@ -0,0 +1,152 @@
+import json
+import random
+from tkinter import *
+
+import model
+
+COLOUR_DATA_FILE_NAME = "colour_data.json"
+VARIABLES_FILE_NAME = "variables.json"
+VARIABLES_COLOUR_DATA_CHANGE_NAME = "colour_data_change"
+
+
+def do_nothing():
+    pass
+
+
+def show_colour(colour, window):
+    window.title("Colour")
+    window.protocol("WM_DELETE_WINDOW", do_nothing)
+    window.geometry("400x400")
+    window.configure(background=colour)
+    window.resizable(0, 0)
+    window.attributes("-topmost", True)
+    window.after(5000, window.destroy)
+    window.mainloop()
+
+
+def read_colour_data():
+    colour_data = {}
+
+    try:
+        colour_data_file = open(COLOUR_DATA_FILE_NAME, "r")
+        colour_data = json.load(colour_data_file)
+        colour_data_file.close()
+    except FileNotFoundError:
+        pass
+    except json.decoder.JSONDecodeError:
+        print("{} may have been corrupted. It will be discarded when the next response is recorded."
+              .format(COLOUR_DATA_FILE_NAME))
+
+    return colour_data
+
+
+def read_variables_data():
+    variables_data = {}
+    try:
+        variables_data_file = open(VARIABLES_FILE_NAME, "r")
+        variables_data = json.load(variables_data_file)
+        variables_data_file.close()
+    except FileNotFoundError:
+        pass
+    except json.decoder.JSONDecodeError:
+        print("{} may have been corrupted. It will be overwritten.".format(VARIABLES_FILE_NAME))
+
+    return variables_data
+
+
+def set_colour_data_change(value):
+    variables_data = read_variables_data()
+    variables_data[VARIABLES_COLOUR_DATA_CHANGE_NAME] = value
+
+    variables_data_file = open(VARIABLES_FILE_NAME, "w+")
+    variables_data_file.write(json.dumps(variables_data))
+    variables_data_file.close()
+
+
+def write_colour_data(colour_data):
+    colour_data_file = open(COLOUR_DATA_FILE_NAME, "w+")
+    colour_data_file.write(json.dumps(colour_data))
+    colour_data_file.close()
+
+    set_colour_data_change(True)
+
+
+def random_colour():
+    return '#%02X%02X%02X' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+def get_dark_light_response():
+    response = input("Options:\n[D]ark\n[L]ight\n").lower()
+    while response != "d" and response != "l":
+        response = input("Invalid input. Type D for dark or L for light\n")
+
+    return response
+
+
+def get_and_log_colour_response(colour):
+    colour_data = read_colour_data()
+    print("Do you think this colour is dark or light?\n(wait to respond)")
+
+    window = Tk()
+    show_colour(colour, window)
+    response = get_dark_light_response()
+
+    colour_data[colour] = int(response == "d")  # 1 if response is d, 0 if l
+    write_colour_data(colour_data)
+
+    print("Response logged\n")
+
+
+def use():
+    variables_data = read_variables_data()
+    loop = True
+
+    try:
+        if variables_data[VARIABLES_COLOUR_DATA_CHANGE_NAME]:
+            print("Updating model...")
+            model.rename_and_replace_model(model.new_trained_model(verbose=2))
+            set_colour_data_change(False)
+    except KeyError:
+        print("No training data found. Train the neural network before using it.")
+        loop = False
+
+    m = model.load_newest_model()
+
+    while loop:
+        colour = random_colour()
+
+        prediction = round(model.prediction_from_colour(colour, m))
+        prediction_text = "dark" if prediction else "light"
+
+        print("The neural network thinks this colour is {}".format(prediction_text))
+
+        get_and_log_colour_response(colour)
+
+        loop = ("q" != input("[Q]uit main menu?\n(Type Q to return or hit enter to continue training)\n").lower())
+
+
+def train():
+    loop = True
+    while loop:
+        colour_data = read_colour_data()
+
+        colour = random_colour()
+        while colour in colour_data.keys():
+            colour = random_colour()
+
+        get_and_log_colour_response(colour)
+
+        loop = ("q" != input("[Q]uit main menu?\n(Type Q to return or hit enter to continue training)\n").lower())
+
+
+while True:
+    choice = input("Main Menu\nOptions:\n[T]rain neural network\n[U]se neural network\n[Q]uit\n").lower()
+    while choice != "t" and choice != "u" and choice != "q":
+        choice = input("Invalid input. Type T to train or U to use or Q to quit\n").lower()
+
+    if choice == "t":
+        train()
+    elif choice == "u":
+        use()
+    elif choice == "q":
+        break
---
+++ model.py
@@ -0,0 +1,96 @@
+import glob
+import json
+import os
+
+import numpy
+from keras.layers import *
+from keras.models import Sequential
+from keras.models import load_model
+
+TRAINING_DATA_FILE_NAME = "colour_data.json"
+
+
+def model_exists():
+    return os.path.exists("model.h5")
+
+
+def delete_and_replace_model(new_model):
+    if os.path.exists("model.h5"):
+        os.remove("model.h5")
+
+    new_model.save("model.h5")
+
+
+def rename_and_replace_model(new_model):
+    if os.path.exists("model.h5"):
+        highest_model_number = 0
+        for filename in glob.glob("model_old*.h5"):
+            model_number = int(filename[9:-3])
+            if model_number < highest_model_number:
+                highest_model_number = model_number
+
+        os.rename("model.h5", "model_old{}.h5".format(highest_model_number + 1))
+
+    new_model.save("model.h5")
+
+
+def create_new_model():
+    model = Sequential()
+    model.add(Dense(3, input_dim=3, activation="relu", name="layer_1"))
+    model.add(Dense(2, activation="relu", name="layer_2"))
+    model.add(Dense(1, activation="linear", name="output_layer"))
+    model.compile(loss="mean_squared_error", optimizer="adam")
+
+    return model
+
+
+def save_new_model():
+    rename_and_replace_model(create_new_model())
+
+
+def load_and_preprocess_data():
+    colour_data = {}
+    try:
+        colour_data_file = open(TRAINING_DATA_FILE_NAME, "r")
+        colour_data = json.load(colour_data_file)
+        colour_data_file.close()
+    except FileNotFoundError:
+        pass
+    except json.decoder.JSONDecodeError:
+        print("Error loading training data. {} may have been corrupted.".format(TRAINING_DATA_FILE_NAME))
+
+    colours, results = [], []
+
+    for colour in colour_data.keys():
+        colours.append(hexcode_to_normalised_array(colour))
+        results.append(float(colour_data[colour]))
+
+    return [numpy.asarray(colours), numpy.asarray(results)]
+
+
+def new_trained_model(verbose=0):
+    model = create_new_model()
+    x, y = load_and_preprocess_data()
+    model.fit(x, y, epochs=10, shuffle=True, verbose=verbose)
+
+    return model
+
+
+def load_newest_model():
+    return load_model("model.h5")
+
+
+def hexcode_to_normalised_array(hexcode):
+    return [int(hexcode.lstrip("#")[i:i + 2], 16) / 255 for i in (0, 2, 4)]
+
+
+# def prediction_from_colours(model, colours):
+#     numpy.asarray([hexcode_to_normalised_array(colour) for colour in colours])
+
+
+def prediction_from_colour(model, colour):
+    x = numpy.asarray([hexcode_to_normalised_array(colour)])
+    return model.predict(x)[0][0]
+
+
+print(prediction_from_colour(load_newest_model(), "#000000"))
